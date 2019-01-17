bd75b19d202f94672bb53121c17862f30dad06ba
Tatiana Scott <hello@tatianascott.com>
2019-01-17 17:46:05 -0500

    deleted button tests

--- audio-trigger-button.js
+++
@@ -1,9 +0,0 @@
-let button = document.getElementById('audio-trigger-button');
-let testVideoOne = document.getElementById('welcomeToSpace');
-let testVideoTwo = document.getElementById('michaelJackson')
-
-button.onClick = function() {
-  console.log('button clicked');
-  testVideoOne.play();
-  testVideoTwo.play();
-}
